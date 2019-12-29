af881277b4582b285345e04744c43f0aa96a1327
Anton-Boldyrev <57538695+Anton-Boldyrev@users.noreply.github.com>
2019-12-29 23:43:49 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,44 +1,44 @@
-#include <sys/types.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/wait.h>
 #include <stdlib.h>
-#include <ctype.h>
-
-int main(int argc, char** argv) {
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(int argc, char** argv)
+{
     pid_t pid;
     int rv;
     int fd[2];
-    
     if (pipe(fd) == -1) {
         perror("pipe error");
         exit(1);
     }
+    switch ( pid = fork() ) {
+        case -1:
+            perror("fork");
+            exit(1);
 
-    if ((pid = fork()) < 0) {
-        perror("fork error");
-        exit(1);
-    } else if (pid == 0) {                // потомок
-        close(fd[0]);                     // потомок не читает
-        dup2(fd[1], STDOUT_FILENO);       // перенаправление stdout
-        rv = execvp(argv[1], argv + 1);
-        if (rv) {
-            perror("exec error");
-        }
-        _exit(rv);
+        case 0:
+            printf(" CHILD: ПроцПотомок, Мой PID -- %d\n", getpid());
+            printf(" CHILD: PID моего родителя -- %d\n", getppid());
+            printf(" CHILD: Введите мой код возврата: ___\b\b\b");
+            close(fd[0]);                     // потомок не читает
+            dup2(fd[1], STDOUT_FILENO);       // перенаправление stdout
+            rv = execvp(argv[1], argv + 1);
+            exit(0);
 
-    } else {                              // родитель
-        waitpid(pid, &rv, 0);
-
-        close(fd[1]);                     // родитель не записывает
-        char buf[1];
-
-        while(read(fd[0], buf, 1) > 0) {
-           if (buf[0] == ' ') {
-                buf[0] = '_'; //замена
-           }
-            write(1, buf, 1);             // в stdout
-        }
-        exit(WEXITSTATUS(rv));
+        default:
+            waitpid(pid, &rv, 0);
+            close(fd[1]);                     // родитель не записывает
+            char buf[1];
+            while(read(fd[0], buf, 1) > 0) {
+                if (buf[0] == ' ') {
+                    buf[0] = '_';   //замена замена пробелов на нижнее подчеркивание
+                }
+                write(1, buf, 1);             // в stdout
+            }
+            exit(WEXITSTATUS(rv));
+            wait(0);
     }
+return 0;
 }
